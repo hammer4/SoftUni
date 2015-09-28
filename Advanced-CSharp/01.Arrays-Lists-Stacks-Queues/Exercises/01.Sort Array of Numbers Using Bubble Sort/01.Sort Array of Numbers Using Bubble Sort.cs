b8d0d93f46c48bc0a51b7c42cf0d0a2ff7069100
hammer4 <hammer3@abv.bg>
2015-09-28 17:50:02 +0300

    new

---
+++ Advanced-CSharp/01.Arrays-Lists-Stacks-Queues/Exercises/01.Sort Array of Numbers Using Bubble Sort/01.Sort Array of Numbers Using Bubble Sort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ArrayofNumbersUsingBubbleSort
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+        int[] integers = numbers.Select(int.Parse).ToArray();
+        BubbleSort(integers);
+        Console.WriteLine(String.Join(", ", integers));
+    }
+
+    static int[] BubbleSort(int[] unsortedArray)
+    {
+        int sortedElements=0;
+        for(int i=0; i<unsortedArray.Length-1; i++)
+        {
+            bool arrayIsSorted = true;
+            for(int j=0; j<unsortedArray.Length-sortedElements-1; j++)
+            {
+                if (unsortedArray[j] > unsortedArray[j + 1])
+                {
+                    int changer = unsortedArray[j];
+                    unsortedArray[j] = unsortedArray[j + 1];
+                    unsortedArray[j + 1] = changer;
+                    arrayIsSorted = false;
+                }
+            }
+            if (arrayIsSorted)
+            {
+                break;
+            }
+            sortedElements++;
+        }
+        return unsortedArray;
+    }
+}
---
+++ Advanced-CSharp/01.Arrays-Lists-Stacks-Queues/Exercises/02.Join Lists/02.Join Lists.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class JoinLists
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+        List<int> list1 = new List<int>();
+        foreach(string number in numbers)
+        {
+            list1.Add(int.Parse(number));
+        }
+        
+        input = Console.ReadLine();
+        numbers = input.Split(' ');
+        List<int> list2 = new List<int>();
+        foreach (string number in numbers)
+        {
+            list2.Add(int.Parse(number));
+        }
+
+        List<int> list3 = new List<int>();
+        foreach(int integer in list1)
+        {
+            if(!list3.Contains(integer))
+            {
+                list3.Add(integer);
+            }
+        }
+
+        foreach (int integer in list2)
+        {
+            if (!list3.Contains(integer))
+            {
+                list3.Add(integer);
+            }
+        }
+
+        list3.Sort();
+        Console.WriteLine(String.Join(" ", list3));
+    }
+}
