1fd29f7e1a1917f762a615e83e8c4843b44a8cc8
hammer4 <hammer3@abv.bg>
2016-05-20 13:47:38 +0300

    Added Git Github Debugging Searching Lab

---
+++ Programming Fundamentals/1.1 Git Github Debugging Searching Lab/01. Debug the Code - Non-Working Days between Two Dates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        var startDate = DateTime.ParseExact(Console.ReadLine(),
+            "d.M.yyyy", CultureInfo.InvariantCulture);
+        var endDate = DateTime.ParseExact(Console.ReadLine(),
+            "d.M.yyyy", CultureInfo.InvariantCulture);
+        var holidaysCount = 0;
+        for (var date = startDate; date <= endDate; date = date.AddDays(1))
+        {
+            if (date.DayOfWeek == DayOfWeek.Saturday ||
+                date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                holidaysCount++;
+            }
+        }
+        Console.WriteLine(holidaysCount);
+    }
+}
