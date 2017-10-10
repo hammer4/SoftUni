f82864d8ff25d8d8bfbe9bb875656845cd0be974
Kiril Georgiev <hammer3@abv.bg>
2017-10-10 08:18:51 +0300

    Added Table Relations Exercises

---
+++ Databases Basics - MSSQL Server/06. Table Relations/06. Table Relations Exercises.sql
@@ -0,0 +1,199 @@
+CREATE DATABASE TR1
+USE TR1
+
+/*--01--*/
+CREATE TABLE Persons (
+	PersonID INT IDENTITY,
+	FirstName VARCHAR(50) NOT NULL,
+	Salary DECIMAL(7, 2),
+	PassportID INT
+)
+
+CREATE TABLE Passports (
+	PassportID INT IDENTITY(101, 1),
+	PassportNumber VARCHAR(50) NOT NULL
+)
+
+INSERT INTO Persons VALUES
+('Roberto', 43300, 102),
+('Tom', 56100, 103),
+('Yana', 60200, 101)
+
+INSERT INTO Passports VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+ALTER TABLE Persons
+ADD PRIMARY KEY (PersonID)
+
+ALTER TABLE Passports
+ADD PRIMARY KEY (PassportID)
+
+ALTER TABLE Persons
+ADD FOREIGN KEY (PassportID) REFERENCES Passports(PassportID)
+
+/*--02--*/
+CREATE TABLE Manufacturers (
+	ManufacturerID INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(30) NOT NULL,
+	EstablishedOn DATE
+)
+
+CREATE TABLE Models (
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	Name VARCHAR(30) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
+)
+
+INSERT INTO Manufacturers VALUES
+('BMW', '1916-03-07'),
+('Tesla', '2003-01-01'),
+('Lada', '1966-05-01')
+
+INSERT INTO Models VALUES
+('X1', 1),
+('i6', 1),
+('Model S', 2),
+('Model X', 2),
+('Model 3', 2),
+('Nova', 3)
+
+/*--03--*/
+CREATE TABLE Students (
+	StudentID INT PRIMARY KEY IDENTITY,
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams (
+	ExamID INT PRIMARY KEY IDENTITY(101, 1),
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE StudentsExams (
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	ExamID INT FOREIGN KEY REFERENCES Exams(ExamID),
+	CONSTRAINT PK_Students_Exams PRIMARY KEY(StudentID, ExamID)
+)
+
+INSERT INTO Students VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
+
+/*--04--*/
+CREATE TABLE Teachers (
+	TeacherID INT PRIMARY KEY IDENTITY(101, 1),
+	Name VARCHAR(50) NOT NULL,
+	ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+)
+
+INSERT INTO Teachers VALUES
+('John', NULL),
+('Maya', NULL),
+('Silvia', NULL),
+('Ted', NULL),
+('Mark', 101),
+('Greta', 101)
+
+UPDATE Teachers
+SET ManagerID = 106
+WHERE TeacherID = 102 OR TeacherID = 103
+
+UPDATE Teachers
+SET ManagerID = 105
+WHERE TeacherID = 104
+
+/*--05--*/
+CREATE DATABASE TR5
+USE TR5
+
+CREATE TABLE Cities(
+	CityID INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Customers(
+	CustomerID INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(50) NOT NULL,
+	Birthday DATE,
+	CityID INT FOREIGN KEY REFERENCES Cities(CityID) NOT NULL
+)
+
+CREATE TABLE Orders(
+	OrderID INT PRIMARY KEY NOT NULL,
+	CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID) NOT NULL
+)
+
+CREATE TABLE ItemTypes(
+	ItemTypeID INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Items (
+	ItemID INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(50) NOT NULL,
+	ItemTypeID INT FOREIGN KEY REFERENCES ItemTypes(ItemTypeID) NOT NULL
+)
+
+CREATE TABLE OrderItems (
+	OrderID INT FOREIGN KEY REFERENCES Orders(OrderID) NOT NULL,
+	ItemID INT FOREIGN KEY REFERENCES Items(ItemID) NOT NULL
+	CONSTRAINT PK_Order_Items PRIMARY KEY (OrderID, ItemID)
+)
+
+/*--06--*/
+CREATE DATABASE TR6
+GO
+
+USE TR6
+
+CREATE TABLE Majors (
+	MajorID INT PRIMARY KEY NOT NULL,
+	Name VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Students (
+	StudentID INT PRIMARY KEY NOT NULL,
+	StudentNumber VARCHAR(15) NOT NULL,
+	StudentName VARCHAR(50) NOT NULL,
+	MajorID INT FOREIGN KEY REFERENCES Majors(MajorID)
+)
+
+CREATE TABLE Payments (
+	PaymentID INT PRIMARY KEY NOT NULL,
+	PaymentDate DATETIME NOT NULL,
+	PaymentAmount DECIMAL(6, 2) NOT NULL,
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID)
+)
+
+CREATE TABLE Subjects (
+	SubjectID INT PRIMARY KEY NOT NULL,
+	SubjectName VARCHAR(35) NOT NULL
+)
+
+CREATE TABLE Agenda (
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	SubjectID INT FOREIGN KEY REFERENCES Subjects(SubjectID)
+	CONSTRAINT PK_Agenda PRIMARY KEY (StudentID, SubjectID)
+)
+
+/*--09--*/
+USE Geography
+SELECT m.MountainRange, p.PeakName, p.Elevation FROM Mountains AS m
+JOIN Peaks AS p ON p.MountainId = m.Id
+WHERE m.MountainRange = 'Rila'
+ORDER BY p.Elevation DESC
