3dc6b3423afa04678786109c2c8a81d3d9e4312b
Kiril Georgiev <hammer3@abv.bg>
2018-01-09 22:48:44 +0200

    Added Linear Data Structures - Lists - Lab

---
+++ Data Structures/01. Linear Data Structures - Lab/Lists/ArrayList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private const int Initial_Capacity = 2;
+    private T[] items;
+
+    public ArrayList()
+    {
+        this.items = new T[Initial_Capacity];
+    }
+
+    public int Count { get; private set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            if(index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return this.items[index];
+        }
+
+        set
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            this.items[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if(this.Count == this.items.Length)
+        {
+            this.Resize();
+        }
+
+        this.items[this.Count++] = item;
+    }
+
+    private void Resize()
+    {
+        T[] copy = new T[this.items.Length * 2];
+        Array.Copy(this.items, copy, this.Count);
+        this.items = copy;
+    }
+
+    public T RemoveAt(int index)
+    {
+        if(index < 0 || index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        T element = this.items[index];
+        this.items[index] = default(T);
+        this.Shift(index);
+        this.Count--;
+
+        if(this.Count <= this.items.Length / 4)
+        {
+            this.Shrink();
+        }
+
+        return element;
+    }
+
+    private void Shrink()
+    {
+        T[] copy = new T[this.items.Length / 2];
+        Array.Copy(this.items, copy, this.Count);
+        this.items = copy;
+    }
+
+    private void Shift(int index)
+    {
+        for(int i = index; i < this.Count; i++)
+        {
+            this.items[i] = this.items[i + 1];
+        }
+    }
+}
---
+++ Data Structures/01. Linear Data Structures - Lab/Tests.ArrayList/ArrayListTests.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+[TestClass]
+public class ArrayListTests
+{
+    [TestMethod]
+    public void AddSingleElementShouldIncreaseCount()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        list.Add(1);
+
+        Assert.AreEqual(1, list.Count);
+    }
+
+    [TestMethod]
+    public void AddMultipleElementShouldIncreaseCount()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            list.Add(i);
+        }
+
+        Assert.AreEqual(100, list.Count);
+    }
+
+    [TestMethod]
+    public void AddAndGetSingleElement()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        list.Add(5);
+
+        Assert.AreEqual(5, list[0]);
+    }
+
+    [TestMethod]
+    public void AddAndGetMultipleElements()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            list.Add(i);
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            Assert.AreEqual(i, list[i]);
+        }
+    }
+
+    [TestMethod]
+    public void SetSingleElement()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        list.Add(0);
+        list[0] = 2;
+
+        Assert.AreEqual(2, list[0]);
+    }
+
+    [TestMethod]
+    public void SetMultipleElements()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            list.Add(i);
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i] = i + 1;
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            Assert.AreEqual(i + 1, list[i]);
+        }
+    }
+
+    [TestMethod]
+    public void RemoveSingleElementShouldHaveCorrectCount()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        list.Add(0);
+        list.RemoveAt(0);
+
+        Assert.AreEqual(0, list.Count);
+    }
+
+    [TestMethod]
+    public void RemoveSingleElementShouldHaveCorrectElements()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        list.Add(0);
+        list.Add(1);
+        list.Add(2);
+        list.RemoveAt(0);
+
+        Assert.AreEqual(1, list[0]);
+        Assert.AreEqual(2, list[1]);
+    }
+
+    [TestMethod]
+    public void RemoveMultipleElements()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            list.Add(i);
+        }
+
+        for (int i = 0; i < 50; i++)
+        {
+            list.RemoveAt(0);
+        }
+
+        Assert.AreEqual(50, list.Count);
+        for (int i = 0; i < 50; i++)
+        {
+            Assert.AreEqual(i + 50, list[i]);
+        }
+    }
+
+    [TestMethod]
+    public void AddMultiplePerformanceTest()
+    {
+        ArrayList<int> list = new ArrayList<int>();
+
+        for (int i = 0; i < 100000; i++)
+        {
+            list.Add(i);
+        }
+
+        for (int i = 0; i < 100000; i++)
+        {
+            Assert.AreEqual(i, list[i]);
+        }
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void SetInvalidPositionShouldThrow()
+    {
+        var list = new ArrayList<int>();
+
+        list[5] = 2;
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void RemoveAtInvalidPositionShouldThrow()
+    {
+        var list = new ArrayList<int>();
+
+        list.RemoveAt(3);
+    }
+}
