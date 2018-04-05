6d9915185df92ad053a520178a210658c740a8fb
Kiril Georgiev <hammer3@abv.bg>
2018-04-05 20:46:58 +0300

    Added Dynamic Programming Part II Lab

---
+++ Algorithms/06. Dynamic Programming Part II - Lab/01. Knapsack/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int capacity = int.Parse(Console.ReadLine());
+
+        string command;
+
+        List<Item> itemsList = new List<Item>();
+        while ((command = Console.ReadLine()) != "end")
+        {
+            var tokens = command.Split();
+
+            itemsList.Add(new Item { Name = tokens[0], Weight = int.Parse(tokens[1]), Value = int.Parse(tokens[2]) });
+        }
+
+        Item[] items = itemsList.OrderBy(i => i.Name).ToArray();
+
+        var result = FillKnapsack(items, capacity);
+
+        Console.WriteLine($"Total Weight: {result.Sum(i => i.Weight)}");
+        Console.WriteLine($"Total Value: {result.Sum(i => i.Value)}");
+        Console.WriteLine(String.Join(Environment.NewLine, result.Select(i => i.Name)));
+    }
+
+    static List<Item> FillKnapsack(Item[] items, int capacity)
+    {
+        var maxValues = new int[items.Length + 1, capacity + 1];
+        var itemIncluded = new bool[items.Length + 1, capacity + 1];
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            for (int currCapacity = 1; currCapacity <= capacity; currCapacity++)
+            {
+                if (items[i].Weight > currCapacity)
+                {
+                    continue;
+                }
+
+                int valueIncluded = items[i].Value + maxValues[i, currCapacity - items[i].Weight];
+                if (valueIncluded > maxValues[i, currCapacity])
+                {
+                    maxValues[i + 1, currCapacity] = valueIncluded;
+                    itemIncluded[i + 1, currCapacity] = true;
+                }
+                else
+                {
+                    maxValues[i + 1, currCapacity] = maxValues[i, currCapacity];
+                }
+            }
+        }
+
+        List<Item> takenItems = new List<Item>();
+        for (int i = items.Length; i > 0; i--)
+        {
+            if (!itemIncluded[i, capacity]) continue;
+
+            Item item = items[i - 1];
+            takenItems.Add(item);
+
+            capacity -= item.Weight;
+        }
+
+        takenItems.Reverse();
+        return takenItems;
+    }
+
+    private class Item
+    {
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Value { get; set; }
+    }
+}
---
+++ Algorithms/06. Dynamic Programming Part II - Lab/02. Longest Common Subsequence/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static int[,] lcs;
+    static string firstStr;
+    static string secondStr;
+
+    static void Main(string[] args)
+    {
+        firstStr = Console.ReadLine();
+        secondStr = Console.ReadLine();
+        lcs = new int[firstStr.Length, secondStr.Length];
+
+        CalcLCS(firstStr.Length - 1, secondStr.Length - 1);
+        Console.WriteLine(lcs[lcs.GetLength(0) - 1, lcs.GetLength(1) - 1]);
+    }
+
+    static int CalcLCS(int x, int y)
+    {
+        if (x < 0 || y < 0)
+            return 0;
+        if (lcs[x, y] == 0)
+        {
+            int lcsFirstMinusOne = CalcLCS(x - 1, y);
+            int lcsSecondMinusOne = CalcLCS(x, y - 1);
+            lcs[x, y] = Math.Max(
+              lcsFirstMinusOne, lcsSecondMinusOne);
+            if (firstStr[x] == secondStr[y])
+            {
+                int lcsBothMinusOne = 1 + CalcLCS(x - 1, y - 1);
+                lcs[x, y] = Math.Max(lcs[x, y], lcsBothMinusOne);
+            }
+        }
+        return lcs[x, y];
+    }
+}
