df473dec156ed8adb36343e52280457170756359
Kiril Georgiev <hammer3@abv.bg>
2018-05-01 19:02:31 +0300

    Added Advanced Graph Algorithms Part II Exercises Problems 01 02

---
+++ Algorithms/09. Advanced Graph Algorithms Part II - Exercises/01. Maximum Tasks Assignment/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static int[][] graph;
+    private static int[] parents;
+
+    static void Main(string[] args)
+    {
+        int people = int.Parse(Console.ReadLine().Split().Last());
+        int tasks = int.Parse(Console.ReadLine().Split().Last());
+
+        int nodes = people + tasks + 2;
+        graph = new int[nodes][];
+
+        for(int i=0; i<graph.Length; i++)
+        {
+            graph[i] = new int[nodes];
+        }
+
+        for(int i=0; i<people; i++)
+        {
+            graph[0][i + 1] = 1;
+        }
+
+        for(int i=0; i<tasks; i++)
+        {
+            graph[i + people + 1][graph.Length - 1] = 1;
+        }
+
+        for(int i=0; i<people; i++)
+        {
+            var line = Console.ReadLine();
+
+            for (int j=0; j<tasks; j++)
+            {
+
+                if(line[j] == 'Y')
+                {
+                    graph[i + 1][j + people + 1] = 1;
+                }
+            }
+        }
+
+        FindMaxFlow();
+
+        var queue = new Queue<int>();
+        var result = new SortedSet<string>();
+        var visited = new bool[graph.Length];
+
+        int start = 0;
+        int end = graph.Length - 1;
+
+        queue.Enqueue(end);
+
+        while(queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            visited[node] = true;
+
+            for(int i=0; i < graph.Length; i++)
+            {
+                if(graph[node][i] > 0 && !visited[i])
+                {
+                    queue.Enqueue(i);
+                    visited[i] = true;
+
+                    if(node != end && node != start && i != end && i != start)
+                    {
+                        result.Add($"{(char)(i - 1 + 'A')}-{node - people}");
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(String.Join(Environment.NewLine, result));
+    }
+
+    public static void FindMaxFlow()
+    {
+        parents = new int[graph.Length];
+
+        for (int i = 0; i < graph.Length; i++)
+        {
+            parents[i] = -1;
+        }
+
+        int maxFlow = 0;
+        int start = 0;
+        int end = graph.Length - 1;
+
+        while (BFS(start, end))
+        {
+            int currentNode = end;
+
+            while (currentNode != start)
+            {
+                int previousNode = parents[currentNode];
+
+                graph[previousNode][currentNode] = 0;
+                graph[currentNode][previousNode] = 1;
+
+                currentNode = previousNode;
+            }
+        }
+    }
+
+    private static bool BFS(int start, int end)
+    {
+        bool[] visited = new bool[graph.Length];
+
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(start);
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+            visited[current] = true;
+
+            for (int child = 0; child < graph[current].Length; child++)
+            {
+                if (!visited[child] && graph[current][child] > 0)
+                {
+                    queue.Enqueue(child);
+                    parents[child] = current;
+                }
+            }
+        }
+
+        return visited[end];
+    }
+}
---
+++ Algorithms/09. Advanced Graph Algorithms Part II - Exercises/02. Find Bi-Connected Components/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BiconnectedComponents
+{
+    static bool[] visited;
+    static int[] depth;
+    static int[] lowpoint;
+    static int[] parent;
+    static List<int>[] graph;
+    static int counter;
+    static Stack<KeyValuePair<int, int>> biconnectedConnectedComponents;
+
+    public static void Main(string[] args)
+    {
+        int nodes = int.Parse(Console.ReadLine().Substring(7));
+        int edges = int.Parse(Console.ReadLine().Substring(7));
+        visited = new bool[nodes];
+        depth = new int[nodes];
+        lowpoint = new int[nodes];
+        parent = new int[nodes];
+        biconnectedConnectedComponents = new Stack<KeyValuePair<int, int>>();
+
+        for (int i = 0; i < nodes; i++)
+        {
+            parent[i] = -1;
+        }
+
+        graph = new List<int>[nodes];
+        for (int i = 0; i < nodes; i++)
+        {
+            graph[i] = new List<int>();
+        }
+
+        for (int i = 0; i < edges; i++)
+        {
+            string[] edgeTokens = Console.ReadLine().Split();
+            int parent = int.Parse(edgeTokens[0]);
+            int child = int.Parse(edgeTokens[1]);
+
+            graph[parent].Add(child);
+            graph[child].Add(parent);
+        }
+
+        FindBiconnectedComponents(0, 1);
+        Console.WriteLine($"Number of bi-connected components: {counter}");
+    }
+
+    private static void FindBiconnectedComponents(int node, int d)
+    {
+        visited[node] = true;
+        depth[node] = d;
+        lowpoint[node] = d;
+
+        foreach (var childNode in graph[node])
+        {
+            if (!visited[childNode])
+            {
+                parent[childNode] = node;
+                FindBiconnectedComponents(childNode, d + 1);
+
+                biconnectedConnectedComponents.Push(new KeyValuePair<int, int>(node, childNode));
+                if (lowpoint[childNode] >= depth[node])
+                {
+                    //Console.Write("{ ");
+                    counter++;
+                    if (biconnectedConnectedComponents.Count > 0)
+                    {
+                        var edge = biconnectedConnectedComponents.Peek();
+                        //Console.Write(edge.Key + " ");
+                        do
+                        {
+                            edge = biconnectedConnectedComponents.Pop();
+                            //Console.Write("{0} ", edge.Value);
+
+                        } while (biconnectedConnectedComponents.Count > 0 && (edge.Key != node || edge.Value == biconnectedConnectedComponents.Peek().Key));
+
+
+                        //Console.WriteLine(" }");
+                    }
+
+
+
+                }
+
+                lowpoint[node] = Math.Min(lowpoint[node], lowpoint[childNode]);
+            }
+            else if (childNode != parent[node])
+            {
+                lowpoint[node] = Math.Min(lowpoint[node], depth[childNode]);
+            }
+        }
+    }
+}
