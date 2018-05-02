52a7e46037ff7e11ad74feb9e6d073bea6c4ad6c
Kiril Georgiev <hammer3@abv.bg>
2018-05-03 02:04:32 +0300

    Added Problem Solving Methodology Lab

---
+++ Algorithms/10. Problem Solving Methodology - Lab/01. Blocks/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private const int LettersToChoose = 4;
+    private static readonly HashSet<string> UsedCombinations = new HashSet<string>();
+
+    static void Main(string[] args)
+    {
+        int numberOfLetters = int.Parse(Console.ReadLine());
+
+        var results = FindBlocks(numberOfLetters);
+
+        PrintBlocks(results);
+    }
+
+    private static void PrintBlocks(HashSet<string> results)
+    {
+        Console.WriteLine($"Number of blocks: {results.Count}");
+
+        foreach(var combination in results)
+        {
+            Console.WriteLine(combination);
+        }
+    }
+
+    private static void FillLetters(int numberOfLetters, char[] letters)
+    {
+        for(int i=0; i<numberOfLetters; i++)
+        {
+            letters[i] = (char)('A' + i);
+        }
+    }
+
+    public static HashSet<string> FindBlocks(int numberOfLetters)
+    {
+        var letters = new char[numberOfLetters];
+        FillLetters(numberOfLetters, letters);
+
+        bool[] used = new bool[numberOfLetters];
+        char[] currentCombination = new char[LettersToChoose];
+        HashSet<string> results = new HashSet<string>();
+
+        GenerateVariations(letters, currentCombination, used, results);
+
+        return results;
+    }
+
+    private static void GenerateVariations(char[] letters, char[] currentCombination, bool[] used, HashSet<string> results, int index = 0)
+    {
+        if(index >= currentCombination.Length)
+        {
+            AddResult(currentCombination, results);
+        }
+        else
+        {
+            for(int i=0; i<letters.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    currentCombination[index] = letters[i];
+                    GenerateVariations(letters, currentCombination, used, results, index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+
+    }
+
+    private static void AddResult(char[] result, HashSet<string> results)
+    {
+        string currentCombination = new string(result);
+
+        if (!UsedCombinations.Contains(currentCombination))
+        {
+            results.Add(currentCombination);
+
+            UsedCombinations.Add(currentCombination);
+            UsedCombinations.Add(new string(new[] { result[3], result[0], result[2], result[1] }));
+            UsedCombinations.Add(new string(new[] { result[2], result[3], result[1], result[0] }));
+            UsedCombinations.Add(new string(new[] { result[1], result[2], result[0], result[3] }));
+        }
+    }
+}
---
+++ Algorithms/10. Problem Solving Methodology - Lab/02. Zig-Zag Matrix/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberOfRows = int.Parse(Console.ReadLine());
+        int numberOfColumns = int.Parse(Console.ReadLine());
+
+        int[][] matrix = new int[numberOfRows][];
+        ReadMatrix(numberOfRows, matrix);
+
+        int[,] maxPaths = new int[numberOfRows, numberOfColumns];
+        int[,] previousRowIndex = new int[numberOfRows, numberOfColumns];
+
+        for(int row=1; row<numberOfRows; row++)
+        {
+            maxPaths[row, 0] = matrix[row][0];
+        }
+
+        for(int col = 1; col < numberOfColumns; col++)
+        {
+            for(int row = 0; row < numberOfRows; row++)
+            {
+                int previousMax = 0;
+
+                if(col % 2 != 0)
+                {
+                    for(int i = row + 1; i<numberOfRows; i++)
+                    {
+                        if(maxPaths[i, col - 1] > previousMax)
+                        {
+                            previousMax = maxPaths[i, col - 1];
+                            previousRowIndex[row, col] = i;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < row; i++)
+                    {
+                        if (maxPaths[i, col - 1] > previousMax)
+                        {
+                            previousMax = maxPaths[i, col - 1];
+                            previousRowIndex[row, col] = i;
+                        }
+                    }
+                }
+
+                maxPaths[row, col] = previousMax + matrix[row][col];
+            }
+        }
+
+        var currentRowIndex = GetLAstRowIndexOfPath(maxPaths, numberOfColumns);
+
+        var path = RecoverMaxPath(numberOfColumns, matrix, currentRowIndex, previousRowIndex);
+        Console.WriteLine($"{path.Sum()} = {String.Join(" + ", path)}");
+    }
+
+    private static List<int> RecoverMaxPath(int numberOfColumns, int[][] matrix, int currentRowIndex, int[,] previousRowIndex)
+    {
+        List<int> path = new List<int>();
+        int columnIndex = numberOfColumns - 1;
+
+        while(columnIndex >= 0)
+        {
+            path.Add(matrix[currentRowIndex][columnIndex]);
+            currentRowIndex = previousRowIndex[currentRowIndex, columnIndex];
+
+            columnIndex--;
+        }
+
+        path.Reverse();
+
+        return path;
+    }
+
+    private static int GetLAstRowIndexOfPath(int[,] maxPaths, int numberOfColumns)
+    {
+        int currentRowIndex = -1;
+        int globalMax = 0;
+        for(int row = 0; row < maxPaths.GetLength(0); row++)
+        {
+            if(maxPaths[row, numberOfColumns - 1] > globalMax)
+            {
+                globalMax = maxPaths[row, numberOfColumns - 1];
+                currentRowIndex = row;
+            }
+        }
+
+        return currentRowIndex;
+    }
+
+    private static void ReadMatrix(int numberOfRows, int[][] matrix)
+    {
+        for(int i=0; i<numberOfRows; i++)
+        {
+            matrix[i] = Console.ReadLine()
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
